1bfeee426c6eab271b88e9f8030c277404129990
unknown <you@example.com>
2020-06-30 09:53:20 +0100

    initial commit

---
+++ venv/Lib/site-packages/da_vinci/formats.py
@@ -0,0 +1,20 @@
+
+# Maps formats to Pillow's accepted image formats
+MAPPING = {
+    'jpg': 'JPEG',
+    'jpeg': 'JPEG',
+    'gif': 'GIF',
+    'png': 'PNG',
+    'pdf': 'PDF',
+    'tif': 'TIFF',
+    'tiff': 'TIFF',
+}
+
+EXTENSIONS = {
+    'JPEG': 'jpg',
+    'GIF': 'gif',
+    'PNG': 'png',
+    'PDF': 'pdf',
+    'TIFF': 'tif',
+    'BMP': 'bmp'
+}
